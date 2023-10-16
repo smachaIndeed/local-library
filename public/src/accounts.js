e770d7a736860d6c78b22f0b1f17c02ac4c1d615
smachaIndeed <146054322+smachaIndeed@users.noreply.github.com>
2023-10-16 11:05:53 +0530

    Add files via upload
    Uploading working project

---
+++ public/src/accounts.js
@@ -0,0 +1,49 @@
+function findAccountById(accounts, id) {
+  // YOUR SOLUTION HERE
+ return accounts.find((account) => account.id === id)
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+}
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  accounts.sort((account1, account2) => account1.name.last > account2.name.last? 1 : -1  )
+  return accounts;
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+}
+
+function getAccountFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  return accounts.map((account) => account.name.first+" "+account.name.last )
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
